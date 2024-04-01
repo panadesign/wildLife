6339da97f3adc19f3f775eacd2651ba857d21774
Jeremy <jeremy.charpentier@posteo.net>
2024-04-01 15:31:29 +0200

    initializeSpringBootProject
    add files for springboot project

---
+++ src/test/java/com/wildLife/WildLife/WildLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildLife.WildLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
